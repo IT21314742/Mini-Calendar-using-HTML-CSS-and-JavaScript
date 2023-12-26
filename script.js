b8e6d020639d815f7a8cbac68c309d9447e3dbbd
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2023-12-26 10:00:59 +0530

    update JS
    change Date

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
 
-const date = new date();
+const date = new Date();
