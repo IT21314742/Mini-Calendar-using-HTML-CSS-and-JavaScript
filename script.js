7386d947f332f2a354dba342a15a022f0e2086b2
Vihan Jayasinghe <100029985+IT21314742@users.noreply.github.com>
2023-12-26 09:58:19 +0530

    update JS
    add JS

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const dayNameEl = document.getElementById("day-name");
 const dayNumEl = document.getElementById("day-number");
 const yearEl = document.getElementById("year");
 
-const date 
+const date = new date()
